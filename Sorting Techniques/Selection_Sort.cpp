442f54c5de51939ffa932d92fb7daf58ba58dcbe
Parth <parthtejasvi18@gmail.com>
2024-01-14 19:48:38 +0530

    first commit

---
+++ Sorting Techniques/Selection_Sort.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Selection_sort(int arr[] , int n)
+{
+    for(int i=0;i<=n-2;i++)
+    {
+        int mini = i;
+        for(int j=i;j<=n-1;j++)
+        {
+            if(arr[j]<arr[mini])
+            {
+                mini = j;
+            }
+           
+        }
+         int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i =0;i<n;i++)
+    cin>>arr[i];
+    Selection_sort(arr,n);
+    for(int i =0;i<n;i++)
+    {
+         cout<<arr[i]<<" ";
+    }
+    return 0;
+}
